9fad7cbed3969e14219ce6d024d7585449bdd80d
unknown <cynthiamedina18.08@gmail.com>
2023-12-18 20:54:45 -0300

    clase 10 y 11

--- clase-10/ejemplos/express-socket/src/public/js/main.js
+++ clase-10/ejemplos/express-socket/src/public/js/main.js
@@ -7,18 +7,22 @@ const socketClient = io();
 // Para enviar un mensaje desde el cliente hacia el servidor (o desde el servidor al cliente, recuerda que es bidireccional), utilizamos la palabra emit, el cual debe contar con el ID del mensaje a enviar, seguido del contenido de dicho mensaje.
 // Los IDs deben coincidir para que el mensaje llegue correctamente
 // Finalmente, en nuestro cliente en index.js escribiremos nuestro primer emit:
+
 socketClient.emit("message", "Mensaje desde el cliente");
 
 
 ////escucho el mensaje del sever 1
+
 socketClient.on("server_message", (data) => {
   console.log(data);
 });
 //escucho el mensaje del sever 2
+
 socketClient.on("message_all", (data) => {
   console.log(data);
 });
 //escucho el mensaje del sever 3
+
 socketClient.on("message_all_2", (data) => {
   console.log(data);
 });
