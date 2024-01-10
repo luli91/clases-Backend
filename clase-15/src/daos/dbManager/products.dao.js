982d25e970e1d8bf8a2131c8778517cdb10c3975
unknown <cynthiamedina18.08@gmail.com>
2024-01-10 16:19:50 -0300

    clase 15

---
+++ clase-15/src/daos/dbManager/products.dao.js
@@ -0,0 +1,27 @@
+//DAO es una clase que gestiona los productos de la base de datpos
+
+import { productModel } from "../../models/products.model.js";
+
+class ProductDao {
+  async getAllProducts() {
+    return await productModel.find();
+  }
+
+  async getProductById(id) {
+    return await productModel.findById(id);
+  }
+
+  async createProduct(product) {
+    return await productModel.create(product);
+  }
+
+  async updateProduct(id, product) {
+    return await productModel.findByIdAndUpdate(id, product);
+  }
+
+  async deleteProduct(id) {
+    return await productModel.findByIdAndDelete(id);
+  }
+}
+
+export default new ProductDao();
